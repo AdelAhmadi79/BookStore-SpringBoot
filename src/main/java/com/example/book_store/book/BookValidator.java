fbe51480e77e669fb1f072ad80a195155c48d21b
Adel J A <adel.j.ahmadi@gmail.com>
2023-08-07 11:34:46 +0330

    Adding A Book Input validator for just figure out the best way to Implement it.

--- src/main/java/com/example/book_store/book/BookValidator.java
+++ src/main/java/com/example/book_store/book/BookValidator.java
@@ -3,7 +3,7 @@ package com.example.book_store.book;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class BookValidator {
-    
+
     public boolean validationOfTitleOrPublisher(BookRequest bookRequest){
         return (bookRequest.getAuthor() != null || bookRequest.getPublisher() != null);
 
